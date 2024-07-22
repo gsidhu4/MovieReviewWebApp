405f68d1941ee300fcc35a1ee21e74020d5293a3
Sunny <gsidhu4@asu.edu>
2024-07-22 10:04:58 -0700

    initial commit

---
+++ movies/src/main/java/sidhu/dev/movies/ReviewRepository.java
@@ -0,0 +1,8 @@
+package sidhu.dev.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReviewRepository extends MongoRepository <Review, ObjectId> {
+
+}
