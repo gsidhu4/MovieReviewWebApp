1d1f113b919bdaa8cfa100c27ca1923262d16952
Sunny Sidhu <153118749+gsidhu4@users.noreply.github.com>
2024-08-26 12:26:56 -0700

    Update MovieistApplication.java

--- movies/src/main/java/sidhu/dev/movies/MovieistApplication.java
+++ movies/src/main/java/sidhu/dev/movies/MovieistApplication.java
@@ -2,28 +2,10 @@ package sidhu.dev.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @SpringBootApplication
 public class MovieistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MovieistApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("https://movie-client-theta.vercel.app")
-						.allowedMethods("*")
-						.allowedHeaders("*")
-						.allowCredentials(false).maxAge(3600);
-			}
-		};
 	}
 }
