4e4b643a68b6750d0adebb6bfe7842d4ec53404b
Sunny Sidhu <153118749+gsidhu4@users.noreply.github.com>
2024-08-26 10:16:00 -0700

    Update axiosConfig.js

--- MovieClient/src/api/axiosConfig.js
+++ MovieClient/src/api/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://movie-review-web-app-mauve.vercel.app/',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: { 
-    "ngrok-skip-browser-warning": "true",
     "Content-Type": "application/json"
   }
 });
